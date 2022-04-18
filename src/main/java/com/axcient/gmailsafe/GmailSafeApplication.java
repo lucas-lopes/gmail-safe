158896c154e8b388a0350197d1347ee63fe0e695
Lucas Barbosa <lucas.lopesifrn@gmail.com>
2022-04-18 20:32:36 -0300

    Commit initial

---
+++ src/main/java/com/axcient/gmailsafe/GmailSafeApplication.java
@@ -0,0 +1,13 @@
+package com.axcient.gmailsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmailSafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axcient/gmailsafe/GmailSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axcient.gmailsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
